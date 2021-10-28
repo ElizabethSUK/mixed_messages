9fb8022f0fbb71956d40ec146bd585b91afd229f
ElizabethSUK <NathanSUK@gmx.com>
2021-10-28 16:17:22 +0100

    Added enemyGen and enemyActionGen

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,22 +1,55 @@
 
 // Random number generator that can be called by other functions
 const randNumGen = (number) => {
-   return Math.ceil(Math.random() * number);
+   const num =  Math.floor(Math.random() * number);
+   //console.log(num);
+   return num;
 }
 
-// Arrays that contain the messages that will be used to generate the messages. - Probably be moved inside the message generating function
-const randMessageStart = [
-    "There is something after me",
-    "How to solve problems",
-    "What should I do about Bojo",
-    "Jamie fell down a well",
-    "How do we fix child poverty?"
-];
+// Arrays that contain the messages that will be used to generate the messages. - Probably be moved inside the message generating function - Dnd Related
 
-const randMessageEnd = [
-    "Grab the crowbar",
-    "Wait till tomorrow",
-    "Ask James not me",
-    "Nuke it",
-    "Pray to it"
-];
+const enemyGen = () => {
+    const enemyArr = [
+        "Goblins",
+        "Dragons",
+        "Karens",
+        "Nuns",
+        "Ogres",
+        "Strange Spherical objects",
+        "Anti-vaxxers",
+        "Fuzzy Wizards"
+    ];
+    const randEnemy = enemyArr[randNumGen(enemyArr.length)];
+    console.log(randEnemy);
+    return randEnemy;
+}
+
+const enemyActionGen = () => {
+
+    // list of descriptions for some actions
+    const descriptorArr = [
+        "Loudly",
+        "Ominously",
+        "Scarily",
+        "Provacatively",
+        "Aggresively",
+        "Seductively",
+        "Like an American"
+    ];
+    const randDescriptor = descriptorArr[randNumGen(descriptorArr.length)];
+    const actionArr = [
+        `Chanting ${randDescriptor}`,
+        "Chucking spears",
+        `Eating ${randDescriptor}`,
+        "Insulting James",
+        `Pillaging Towns ${randDescriptor}`
+    ];
+    const randAction = actionArr[randNumGen(actionArr.length)];
+    return randAction;
+}
+
+// let i = 0;
+// while (i < 20) {
+//     console.log(enemyActionGen());
+//     i++
+// }
