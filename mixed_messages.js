3cdd3df7cf2dcaedb49de4d6cf1146a7ded4e224
ElizabethSUK <NathanSUK@gmx.com>
2021-10-28 15:46:37 +0100

    Added two arrays containing elements to be chosen randomly

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,5 +1,22 @@
 
-const randomNumGen = (number) => {
+// Random number generator that can be called by other functions
+const randNumGen = (number) => {
    return Math.ceil(Math.random() * number);
 }
 
+// Arrays that contain the messages that will be used to generate the messages. - Probably be moved inside the message generating function
+const randMessageStart = [
+    "There is something after me",
+    "How to solve problems",
+    "What should I do about Bojo",
+    "Jamie fell down a well",
+    "How do we fix child poverty?"
+];
+
+const randMessageEnd = [
+    "Grab the crowbar",
+    "Wait till tomorrow",
+    "Ask James not me",
+    "Nuke it",
+    "Pray to it"
+];
